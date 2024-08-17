aa3533f9897cd69eb4461f8fb1c0138af4bce5e8
Jesna James <jesna@gocodeo.com>
2024-08-17 07:42:24 +0530

    GoCodeo ADD tests

--- shopping_cart/gocodeo_tests/test_discounts.py
+++ shopping_cart/gocodeo_tests/test_discounts.py
@@ -1,63 +1,57 @@
-import unittest
-from unittest.mock import MagicMock
-from shopping_cart.cart import Cart,Item
-from shopping_cart.discounts import Discount
+# happy_path - apply_discount - Apply discount for a regular user with total price above minimum purchase amount
+def test_apply_discount_regular_user(cart, discount):
+    cart.user_type = 'regular'
+    assert discount.apply_discount(cart) == 110.0
 
+# happy_path - apply_bulk_discount - Apply bulk discount on items that meet the quantity requirement
+def test_apply_bulk_discount(cart, discount):
+    discount.apply_bulk_discount(cart, bulk_quantity=1, bulk_discount_rate=0.2)
+    assert cart.items[0]['price'] == 80.0
 
-class TestDiscount(unittest.TestCase):
-    def setUp(self):
-        self.cart = Cart(user_type="regular")
-        self.cart.user_type = "regular"
-        item1 = {"item_id":1, "price":50.0, "name":"Phone", "category":"electronics", "user_type": "premium", "quantity": 2}
-        item2 = {"item_id":2, "price":50.0, "name":"Tab", "category":"electronics", "user_type": "premium", "quantity": 4}
+# happy_path - apply_seasonal_discount - Apply seasonal discount during holiday season
+def test_apply_seasonal_discount(cart, discount):
+    assert discount.apply_seasonal_discount(cart, season='holiday', seasonal_discount_rate=0.1) == 90.0
 
-        self.cart.items = [
-           item1,
-           item2
-        ]
+# happy_path - apply_category_discount - Apply category discount for electronics
+def test_apply_category_discount(cart, discount):
+    discount.apply_category_discount(cart, category='electronics', category_discount_rate=0.1)
+    assert cart.items[0]['price'] == 90.0
 
-    def test_apply_discount_logic_error(self):
-        self.cart.user_type = "premium"
-        discount = Discount(0.1, 50)
-        total_price = discount.apply_discount(self.cart)
-        self.assertAlmostEqual(total_price, 85.0, places=2, msg="Both values should be equal")
+# happy_path - apply_loyalty_discount - Apply loyalty discount for a loyal user with more than 2 years
+def test_apply_loyalty_discount(cart, discount):
+    cart.user_type = 'loyal'
+    assert discount.apply_loyalty_discount(cart, loyalty_years=3, loyalty_discount_rate=0.1) == 90.0
 
-    def test_apply_bulk_discount(self):
-        bulk_discount_rate = 0.20
-        bulk_quantity = 3
-        discount = Discount(0.1, 50)
-        discount.apply_bulk_discount(self.cart, bulk_quantity, bulk_discount_rate)
-        self.assertAlmostEqual(self.cart.items[1]["price"], 50 * 0.8, places=2, msg="Both values should be equal")
+# happy_path - apply_flash_sale_discount - Apply flash sale discount on items that are on sale
+def test_apply_flash_sale_discount(cart, discount):
+    discount.apply_flash_sale_discount(cart, flash_sale_rate=0.2, items_on_sale=[1])
+    assert cart.items[0]['price'] == 80.0
 
-    def test_apply_seasonal_discount_holiday(self):
-        seasonal_discount_rate = 0.20
-        season = "holiday"
-        discount = Discount(0.1, 50)
-        discount.apply_seasonal_discount(self.cart, season, seasonal_discount_rate)
-        self.assertAlmostEqual(self.cart.total_price, (2 * 50 + 4 * 50) * 0.8, places=2, msg="Both values should be equal")
+# edge_case - apply_discount - Apply discount when total price is below minimum purchase amount
+def test_apply_discount_below_minimum(cart, discount):
+    cart.calculate_total_price = Mock(return_value=40)
+    assert discount.apply_discount(cart) == 40.0
 
-    def test_apply_category_discount(self):
-        category_discount_rate = 0.25
-        category = "electronics"
-        discount = Discount(0.1, 50)
-        discount.apply_category_discount(self.cart, category, category_discount_rate)
-        self.assertAlmostEqual(self.cart.items[1]["price"], 50 * 0.75, places=2, msg="Both values should be equal")
+# edge_case - apply_bulk_discount - No discount applied if quantity does not meet requirement
+def test_apply_bulk_discount_no_discount(cart, discount):
+    discount.apply_bulk_discount(cart, bulk_quantity=5, bulk_discount_rate=0.2)
+    assert cart.items[0]['price'] == 100.0
 
-    def test_apply_loyalty_discount(self):
-        loyalty_discount_rate = 0.05
-        loyalty_years = 3
-        self.cart.user_type = "loyal"
-        discount = Discount(0.1, 50)
-        discount.apply_loyalty_discount(self.cart, loyalty_years, loyalty_discount_rate)
-        self.assertAlmostEqual(self.cart.total_price, (2 * 50 + 4 * 50) * 0.95, places=2, msg="Both values should be equal")
+# edge_case - apply_seasonal_discount - No seasonal discount applied if season is not holiday or summer
+def test_apply_seasonal_discount_no_discount(cart, discount):
+    assert discount.apply_seasonal_discount(cart, season='winter', seasonal_discount_rate=0.1) == 100.0
 
-    def test_apply_flash_sale_discount(self):
-        flash_sale_rate = 0.30
-        items_on_sale = [1, 2]
-        discount = Discount(0.1, 50)
-        discount.apply_flash_sale_discount(self.cart, flash_sale_rate, items_on_sale)
-        self.assertAlmostEqual(self.cart.items[0]["price"], 50 * 0.7, places=2, msg="Both values should be equal")
-        self.assertAlmostEqual(self.cart.items[1]["price"], 50 * 0.7, places=2, msg="Both values should be equal")
+# edge_case - apply_category_discount - No category discount applied if category does not match
+def test_apply_category_discount_no_discount(cart, discount):
+    discount.apply_category_discount(cart, category='furniture', category_discount_rate=0.1)
+    assert cart.items[0]['price'] == 100.0
 
-if __name__ == '__main__':
-    unittest.main()
+# edge_case - apply_loyalty_discount - No loyalty discount applied for non-loyal user
+def test_apply_loyalty_discount_no_discount(cart, discount):
+    assert discount.apply_loyalty_discount(cart, loyalty_years=1, loyalty_discount_rate=0.1) == 100.0
+
+# edge_case - apply_flash_sale_discount - No flash sale discount applied if item is not on sale
+def test_apply_flash_sale_discount_no_discount(cart, discount):
+    discount.apply_flash_sale_discount(cart, flash_sale_rate=0.2, items_on_sale=[3])
+    assert cart.items[0]['price'] == 100.0
+
